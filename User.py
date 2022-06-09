8a95eea816d9071b188d7c874cdf31c6f86c9fd9
Timon Deschamps <timon.desch@gmail.com>
2022-06-09 13:15:30 +0200

    Add User class skeleton

---
+++ User.py
@@ -0,0 +1,20 @@
+class User:
+    def __init__(self):
+        self.wordlist = []
+        self.repos = {}
+        self.issues = {}
+
+    def add_words(self, word_list):
+        """Adds words to user's wordlist
+        """
+        pass
+
+    def add_repos(self, repo_list):
+        """Adds repos to user's repos list.
+        """
+        pass
+
+    def update_issues(self):
+        """Updates user's issues list
+        """
+        pass
